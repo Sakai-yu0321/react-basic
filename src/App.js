7cb6009b16751c5dc9aa3daf67ae4cb45ee860bf
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-06-15 10:59:54 +0900

    reactカウント機能

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // stateを定義
+    this.state = {count: 0};
+  }
+
+  #メソッド定義
+  handleClick() {
+    // stateに+1する処理
+    this.setState({count:this.state.count + 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {/* カウント表示 */}
+          {this.state.count}
+        </h1>
+        {/* buttonがクリックされた場合(onClick)にbundleClickを呼び出し */}
+        <button onClick={() =>{this.handleClick()}}>+</button>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
